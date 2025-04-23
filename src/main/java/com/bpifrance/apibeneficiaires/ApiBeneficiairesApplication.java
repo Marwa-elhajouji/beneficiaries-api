ccdd3916070d1ee7d3dddfcb0013dc075a0fbb52
marwa.elhajouji <marwa.elhajouji@emeria.eu>
2025-04-23 14:09:29 +0200

    feat: add application entry point

---
+++ src/main/java/com/bpifrance/apibeneficiaires/ApiBeneficiairesApplication.java
@@ -0,0 +1,13 @@
+package com.bpifrance.apibeneficiaires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBeneficiairesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBeneficiairesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bpifrance/apibeneficiaires/ApiBeneficiairesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpifrance.apibeneficiaires;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBeneficiairesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
